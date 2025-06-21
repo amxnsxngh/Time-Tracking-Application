f3a9314f0d119323e1071110b595d1f6bf4ad5b3
Aman Singh <amansa6@gmail.com>
2025-06-21 23:22:32 +0200

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="text-center">
+            <h1 className="text-3xl font-bold underline center">
+                Time Tracking Application
+            </h1>
+        </div>
+    )
+}
+export default App
